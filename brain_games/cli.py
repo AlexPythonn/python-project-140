16ace34b8fa890581ae9a9855314cfbc10995771
AlexPythonn <alex@AlexPython.localdomain>
2025-08-19 18:13:03 -0500

    agregamos codigo a los archivos .py, instalamos prompt, ruff

---
+++ brain_games/cli.py
@@ -0,0 +1,9 @@
+
+from prompt_toolkit import prompt
+
+
+def welcome_user():
+   
+   name = prompt('May I have your name? ')
+   print(f"Hello, {name}!")
+
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,10 @@
-print("Welcome to the Brain Games!")
 
+from brain_games.cli import welcome_user
 
 def main():
+  print("brain-games")
+  print("Welcome to the Brain Games!")
+  welcome_user()
   
   if __name__ == "__main__":
-    main()
+    main()  
