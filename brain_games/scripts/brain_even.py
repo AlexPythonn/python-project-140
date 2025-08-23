221bd65203a1c9bc6bd5876fe3ad9a22aecad528
AlexPythonn <alex@AlexPython.localdomain>
2025-08-22 20:37:36 -0500

    Este archivo conecta el juego Par o Impar con el Main

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,11 @@
+
+from brain_games.even import even_or_odd
+
+
+def main():
+  print("brain-even")
+  print("Welcome to the Brain Games!")
+  even_or_odd()
+  
+  if __name__ == "__main__":
+    main() 
