e76ff20ced39e72fa321adefb67164f485ba90cc
AlexPythonn <alex@AlexPython.localdomain>
2025-08-25 19:50:30 -0500

    Creamos archivo de nuevo juego y lo agregamos como nueva entrada

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,10 @@
+from brain_games.progression import progre
+
+
+def main():
+    print("brain-progression")
+    print("Welcome to the Brain Games!")
+    progre()
+    
+    if __name__ == "__main__":
+        main()
