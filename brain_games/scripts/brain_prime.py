059321f23ba169930a31c193a65dc8c64becedc0
AlexPythonn <alex@AlexPython.localdomain>
2025-08-27 16:38:07 -0500

    Se crea este archivo que ejecuta el juego numero primo

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,9 @@
+from brain_games.prime import play_prime_game
+
+def main():
+    print("brain-prime\n")
+    print("Welcome to the Brain Games!")
+    play_prime_game()
+
+    if __name__ == "__main__":
+        main()
